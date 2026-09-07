4c8f70e3a360393cea7e4661c7428ca650527db7
Bsilent_0f38k2f4k <bsilent@bsilent.bsilent>
2026-09-07 14:52:31 +0530

    Updated code

--- SEM3_CODE/PHP.bak/02_factorial.php
+++ SEM3_CODE/PHP.bak/02_factorial.php
@@ -1,3 +1,8 @@
 <?php
-
+$number = 5;                         // Number whose factorial is needed
+$factorial =1;
+for ($value=1;$value<=$number;$value++){
+    $factorial=$factorial*$value;
+}  
+echo 'factorial of '. $number.' is : '. $factorial;      // Start multiplication with 1
 ?>
